65a2487516d4f70454d265549dedae3a1e3c613d
hhf1357924680 <hhf1357924680@163.com>
2021-09-26 16:47:11 +0800

    redect

--- process-bindiff-data/binary2llvm.sh
+++ process-bindiff-data/binary2llvm.sh
@@ -1,6 +1,6 @@
 in_dir=../data-raw/bindiff/gnutls-3.5.19/
 in_file=libgnutls.so
-out_dir=./data/gnutls
+out_dir=./data-bin/gnutls
 max_len=511
 
 mkdir -p $out_dir $out_dir/stripped $out_dir/ir $out_dir/json $out_dir/train_json $out_dir/result
@@ -8,3 +8,6 @@ variants=O0-gcc7.5.0-amd64,O1-gcc7.5.0-amd64,O2-gcc7.5.0-amd64,O3-gcc7.5.0-amd64
 python 1_generate_ground_truth.py $in_dir $in_file $variants $out_dir/function_info.json
 python 2_rename_symbols.py $in_dir $in_file $variants $out_dir/stripped
 python 3_bin2llvm.py $out_dir/stripped $in_file $variants $out_dir/ir
+
+
+python retdec-decompiler.py --keep-unreachable-funcs --stop-after bin2llvmir -o /mnt/work/project/OSCAR/data-raw/bindiff/gnutls-3.5.19/O0-gcc7.5.0-amd64/libgnutls.so /mnt/work/project/OSCAR/process-bindiff-data/data/gnutls/stripped/O0-gcc7.5.0-amd64/libgnutls.so
