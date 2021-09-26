3cfe901ea0132b0ff02e7345e26c44d47b6e4590
hhf1357924680 <13592590769@163.com>
2021-09-26 08:50:34 +0000

    redtdec cmd

--- process-bindiff-data/binary2llvm.sh
+++ process-bindiff-data/binary2llvm.sh
@@ -8,3 +8,4 @@ variants=O0-gcc7.5.0-amd64,O1-gcc7.5.0-amd64,O2-gcc7.5.0-amd64,O3-gcc7.5.0-amd64
 python 1_generate_ground_truth.py $in_dir $in_file $variants $out_dir/function_info.json
 python 2_rename_symbols.py $in_dir $in_file $variants $out_dir/stripped
 python 3_bin2llvm.py $out_dir/stripped $in_file $variants $out_dir/ir
+python /mnt/wanyao/guiyi/opt/retdec_4.0/bin/retdec-decompiler.py --keep-unreachable-funcs --stop-after bin2llvmir -o outputs/31.ll 31.a
